8c2bac0c8c8c88e53592db2e5f0cf7891ac0b978
LeT0t0 <tobiasmnpereira@gmail.com>
2026-06-25 13:47:37 -0300

    Altera fonte para Roboto

--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { createStart, createMiddleware } from "@tanstack/react-start";
 
 import { renderErrorPage } from "./lib/error-page";
